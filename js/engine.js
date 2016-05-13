a314e99f549130d72fdce8af951a969c9fdbe6b8
zohoFrank <zuoh14@mails.tsinghua.edu.cn>
2016-05-13 11:02:00 +0800

    refactor: Separate the scoreboard

--- js/engine.js
+++ js/engine.js
@@ -152,6 +152,7 @@ var Engine = (function(global) {
         });
 
         player.render();
+        scoreBoard.render();
     }
 
     /* This function does nothing but it could have been a good place to
